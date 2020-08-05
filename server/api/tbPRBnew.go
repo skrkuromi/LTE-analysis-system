668f8459cce539c878ccc7d0a8fa0d23a157285c
HoJ825375293 <47890081+HoJ825375293@users.noreply.github.com>
2020-08-05 20:45:53 +0800

    -

--- server/api/tbPRBnew.go
+++ server/api/tbPRBnew.go
@@ -5,9 +5,18 @@ import (
 	"server/models"
 )
 
-func QueryPRBInfoByEnodeName(c *gin.Context){
+func QueryPRBInfoByEnodeName(c *gin.Context) {
 	en := c.Query("网元名称")
-
 	info := models.GetPRBInfoByEnodebName(en)
+	c.JSON(200, gin.H{"msg": info})
+}
+
+func QueryAllKPIEnodeb(c *gin.Context){
+	info := models.GetAllKPIEnodeb()
+	c.JSON(200, gin.H{"msg": info})
+}
+
+func QueryAllKPIAtt(c *gin.Context){
+	info := models.GetAllKPIAtt()
 	c.JSON(200, gin.H{"msg": info})
 }
